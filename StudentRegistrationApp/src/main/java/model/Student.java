997eeeaf8a72884a25f9a91cf8c83c76e39eeb75
chera1606 <cherinetderbie@gmail.com>
2025-12-28 22:44:18 +0300

    Add Student.java model class

---
+++ StudentRegistrationApp/src/main/java/model/Student.java
@@ -0,0 +1,27 @@
+package main.java.model;
+
+
+public class Student {
+
+    private String name;
+    private String email;
+    private int year;
+
+    public Student(String name, String email, int year) {
+        this.name = name;
+        this.email = email;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
